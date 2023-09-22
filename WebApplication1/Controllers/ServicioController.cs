4ce94a81039719c7629506c626fbe8d336720734
tovcha <tovcha@DESKTOP-D365OVB>
2023-09-22 18:46:35 -0500

    se agrego servicio

---
+++ WebApplication1/Controllers/ServicioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.ServiceReference1;
+
+namespace WebApplication1.Controllers
+{
+    public class ServicioController : Controller
+    {
+        private ServiceReference1.WsFactClient objWorkManager = new ServiceReference1.WsFactClient();
+
+        // GET: Servicio
+        public ActionResult Index()
+        {
+            try
+            {
+                MessageRequest request = new MessageRequest();
+
+                request.rfcEmisor = "zzz";
+                request.tipoEmisor = TipoEmisor.Direct;
+                request.operacion = Operacion.Cancelacion;
+
+               MessageResponse resp = objWorkManager.ProcessDocument(request);
+
+                Console.WriteLine(resp.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR " + ex.Message);
+            }
+
+            return View();
+        }
+    }
+}
